6919821788a23b07e065820c7db4e2251f581f70
devansien <devansien@gmail.com>
2019-06-13 03:35:04 +1200

    batch update

--- file_reader.py
+++ file_reader.py
@@ -1,21 +1,16 @@
 import os
 
 
-def count_files(dir):
-    # get relative path from the absolute path
-    abs_path = os.path.abspath(os.path.dirname(__file__))
-    rel_path = os.path.join(abs_path, dir)
-
-    # get text files
+def get_files(dir):
     files = []
-    for r, d, f in os.walk(rel_path):
+    path = get_relative_path(dir)
+    for r, d, f in os.walk(path):
         for file in f:
             if '.txt' in file:
                 files.append(os.path.join(r, file))
+    return files
 
-    # print the list of text files (path)
-    # for f in files:
-    #     print(f)
 
-    # print the file count
-    print('\n' + str(len(files)) + ' files need to be processed')
+def get_relative_path(dir):
+    abs_path = os.path.abspath(os.path.dirname(__file__))
+    return os.path.join(abs_path, dir)
--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import nltk
 import file_reader
+import contractions
 
-file_reader.count_files('data\\')
+# get text files
+files = []
+files = file_reader.get_files('data\\')
+print('\n' + str(len(files)) + ' files need to be processed')
+
+# get contents into a document list
+documents = []
+for f in files:
+    with open(f, 'r') as file:
+        documents.append(file.read())
+
+print(documents[0])
